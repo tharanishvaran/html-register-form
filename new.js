383d923d1afd7db851b7305edb0ad35311f7c538
tharanishvaran <tharanishvaranr@gmail.com>
2026-05-07 16:32:20 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,15 @@
+function validateform(){
+    
+var name=document.getElementById("name").value
+var age=document.getElementById("age").value
+var date=document.getElementById("date").value
+if(name==="")
+{
+    alert("enter details")
+}
+else
+{
+    alert("success")
+}
+}
+
